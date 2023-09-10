5fc70c935ba534daf96576c1778e6c633bf587eb
beidaosir <120437613+beidaosir@users.noreply.github.com>
2023-09-10 22:29:37 +0800

    Add files via upload

---
+++ chapter03/Homework04.java
@@ -0,0 +1,23 @@
+
+public class Homework04{
+	public static void main(String[] args) {
+		/*
+		姓名  年龄  成绩  性别  爱好
+		要求：
+		1.用变量将姓名...储存
+		2.使用+
+		3.添加适当注释
+		4.添加转义字符，使用一条语句输出
+		
+		*/
+		String name = "Tom";
+		int age = 18;
+		double score = 90.5;
+		char gender = '男';
+		String hobby = "玩游戏";
+
+		System.out.println("姓名\t年龄\t成绩\t性别\t爱好\n" + name + "\t" +
+		 age + "\t" + score + "\t" + gender + "\t" + hobby);
+
+	}
+}
