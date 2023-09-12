e95e165ebfb7c1e9a7b862b6e5e8eb35c8a9efe6
beidaosir <120437613+beidaosir@users.noreply.github.com>
2023-09-12 22:42:53 +0800

    Add files via upload

---
+++ 实训/2023_9_12/Test03.java
@@ -0,0 +1,12 @@
+
+public class Test03{
+    public static void main(String args[]){
+        int i=4,j=5,k=9,m=5;
+		if(i>j || m<k) 
+		    k++;
+		else 
+		    k--;
+        System.out.println("kµÄÖµÎª"+k);
+    }
+}
+
---
+++ 实训/2023_9_12/Test10.java
@@ -0,0 +1,10 @@
+
+public class Test10{
+	public static void main(String[] args) {
+		
+		double huashi = 234.6;
+		double sheshi = (huashi-32)*5/9;
+		System.out.println("华氏温度" + huashi + "对应的摄氏温度=" + sheshi);
+ 
+	}
+}
