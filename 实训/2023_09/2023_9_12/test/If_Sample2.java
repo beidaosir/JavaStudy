6517c9b8f7a106dec42d71e045125bc0c13144fb
beidaosir <120437613+beidaosir@users.noreply.github.com>
2023-11-24 22:25:38 +0800

    Add files via upload

---
+++ 实训/2023_09/2023_9_12/test/If_Sample2.java
@@ -0,0 +1,12 @@
+public class If_Sample2 {
+    public static void main(String[] args) {
+        int n = 7;
+        if (n % 2 != 0) {
+            System.out.println("n是奇数");
+            System.out.println("条件表达式返回值为真");
+        } else {
+            System.out.println("n不是奇数");
+            System.out.println("条件表达式返回值为假");
+        }
+    }
+}
---
+++ 实训/2023_09/2023_9_12/test/Test11.java
@@ -0,0 +1,9 @@
+
+public class Test11{
+	public static void main(String[] args) {
+		
+		char x = 'A';
+		System.out.println((char)(x+32));
+ 
+	}
+}
