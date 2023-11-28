ecd9d1c8a764b2ff1a35eda42ffe5e7e32b67389
beidaosir <1360860184@qq.com>
2023-11-28 17:40:29 +0800

    添加前端内容
    前端模块依赖太大只上传关键内容

---
+++ 实训/2023_11/2023_11_27/前端src/src/views/AboutView.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <button @click="getById">通过id查询</button>
+    <button @click="getByPage">分页查询</button>
+    <button @click="add">添加</button>
+    <button @click="update">修改</button>
+    <button @click="del">删除</button>
+  </div>
+
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
+
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'AboutView',
+  data() {
+    return {
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+      ]
+    }
+  },
+  methods: {
+    del() {
+      axios({
+        method: 'delete',
+        url: 'http://localhost:8080/emp/7947'
+      }).then((resData) => {
+        console.log(resData);
+      })
+    },
+    getById() {
+      axios({
+        method: 'get',
+        url: 'http://localhost:8080/emp/7844'
+      }).then((resData) => {
+        console.log(resData);
+      })
+    },
+    getByPage() {
+      axios({
+        method: 'post',
+        url: 'http://localhost:8080/emp/page',
+        data: {
+          currentPage: 2,
+          pageNumber: 3,
+          ename: "A"
+        }
+      }).then((resData) => {
+        console.log(resData);
+      })
+    },
+    add() {
+      axios({
+        method: 'post',
+        url: 'http://localhost:8080/emp/save',
+        data: {
+          ename: "张亮",
+          job: "前台",
+          hiredate: "2023-11-27",
+          sal: 5000.0
+        }
+      }).then((resData) => {
+        console.log(resData);
+      })
+    },
+    update() {
+      axios({
+        method: 'post',
+        url: 'http://localhost:8080/emp/save',
+        data: {
+          ename: "张亮",
+          job: "前台",
+          hiredate: "2023-11-27",
+          sal: 5000.0,
+          empno: 7948
+        }
+      }).then((resData) => {
+        console.log(resData);
+      })
+    }
+  },
+  //生命周期钩子函数
+  mounted() {
+
+  }
+} 
+</script>
+
+<style></style>
+
---
+++ 实训/2023_11/2023_11_27/前端src/src/views/EmployeeView.vue
@@ -0,0 +1,169 @@
+<template>
+    <div>
+
+
+        <!-- el-row 行  可以分24份
+            el-col :span="分几份"
+        -->
+        <el-row>
+            <el-col :span="3"></el-col>
+            <el-col :span="4">
+                <el-input v-model="ename" class="w-50 m-2" placeholder="姓名搜索" />
+            </el-col>
+            <el-col :span="1"><el-button type="primary" @click="getPageData">搜索</el-button></el-col>
+            <el-col :span="4">
+                <el-button type="primary" @click="showAddDialog">添加员工</el-button>
+            </el-col>
+        </el-row>
+
+        <el-row>
+            <el-col :span="3"></el-col>
+            <el-col :span="18">
+                <el-table :data="tableData" style="width: 100%">
+                    <el-table-column prop="empno" label="员工编号" />
+                    <el-table-column prop="ename" label="员工姓名" />
+                    <el-table-column prop="job" label="工作岗位" />
+                    <el-table-column prop="hiredate" label="入职日期" />
+                    <el-table-column prop="sal" label="工资收入" />
+                    <el-table-column label="操作">
+                        <template #default="scope">
+                            <el-button size="small" @click="handleEdit(scope.$index, scope.row)"
+                            >编辑</el-button>
+                            <el-button
+                                size="small"
+                                type="danger"
+                                @click="handleDelete(scope.$index, scope.row)"
+                            >删除</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-col>
+            <el-col :span="3"></el-col>
+        </el-row>
+
+        <el-row>
+            <el-col :span="3"></el-col>
+            <el-col :span="18">
+                <!--
+                v-model:current-page="currentPage" 当前页
+                v-model:page-size="pageNumber"  每页显示多少条数据
+                :page-sizes="[3, 5, 8, 10]"   可以切换的每页显示的记录数选项
+                layout="total, sizes,                   prev,     pager, next,        jumper"  
+                布局：总记录 每页记录数的选项下拉列表   上一页箭头  页码   下一页的箭头  可以跳转到某页
+                :total="totalRows"  总记录    使用该数字和pageNumber来做计算
+                @size-change="getPageData"  监听到pageNumber的变化重新查询
+                @current-change="getPageData" 监听到currentPage的变化重新查询
+            -->
+                <el-pagination v-model:current-page="currentPage" v-model:page-size="pageNumber" :page-sizes="[3, 5, 8, 10]"
+                    :small="false" :disabled="false" :background="false" layout="total, sizes, prev, pager, next, jumper"
+                    :total="totalRows" @size-change="getPageData" @current-change="getPageData" />
+            </el-col>
+            <el-col :span="3"></el-col>
+        </el-row>
+
+        <!-- 添加和修改的对话框 -->
+        <el-dialog v-model="dialogVisible" :title="dialogTitle" width="30%" :before-close="handleClose">
+            <div>
+                <el-form :model="form" label-width="120px">
+                    <el-form-item label="员工姓名">
+                        <el-input v-model="form.ename" />
+                    </el-form-item>
+                    <el-form-item label="员工岗位">
+                        <el-input v-model="form.job" />
+                    </el-form-item>
+                    <el-form-item label="入职日期">
+                        <el-date-picker
+                            v-model="form.hiredate"
+                            type="date"
+                        />
+                    </el-form-item>
+                    <el-form-item label="员工工资">
+                        <el-input-number v-model="form.sal" :step="1000" />
+                    </el-form-item>
+                </el-form>    
+            </div>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="dialogVisible = false">取消</el-button>
+                    <el-button type="primary" @click="save">
+                        {{dialogSubmitBt}}
+                    </el-button>
+                </span>
+            </template>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'EmployeeView',
+    data() {
+        return {
+            currentPage: 1, //当前页
+            pageNumber: 5, //每页显示的记录数
+            ename: '', //姓名搜索条件
+            totalRows: 0, //总数量 查询得到
+            tableData: [], //表数据 查询得到
+            dialogVisible: false,  //对话框是否显示
+            dialogTitle: '添加员工信息',
+            dialogSubmitBt: '确认添加',
+            form: {
+                ename: '',
+                job: '',
+                sal: 0,
+                hiredate: ''
+            }
+        }
+    },
+    methods: {
+        //添加或修改
+        save(){
+            //发送请求
+            axios({
+                method: 'post',
+                url: 'http://localhost:8080/emp/save',
+                data: Object.assign({},this.form)
+            }).then(res=>{
+                if(res.data=="success"){
+                    this.getPageData();
+                    this.dialogVisible = false;
+                }
+            })
+        },
+        //显示添加的对话框
+        showAddDialog(){
+            this.dialogTitle = '添加员工信息'
+            this.dialogSubmitBt = '确认添加'
+            this.form =  {
+                ename: '',
+                job: '',
+                sal: 0,
+                hiredate: ''
+            }
+            this.dialogVisible = true
+        },
+        //分页查询
+        getPageData() {
+            axios({
+                method: 'post',
+                url: 'http://localhost:8080/emp/page',
+                data: {
+                    currentPage: this.currentPage,
+                    pageNumber: this.pageNumber,
+                    ename: this.ename
+                }
+            }).then(res => {
+                console.log(res);
+                this.tableData = res.data.data; //表数据
+                this.totalRows = res.data.totalRows; //总记录数
+            })
+        }
+    },
+    mounted() {
+        this.getPageData();
+    }
+} 
+</script>
+
+<style></style>
