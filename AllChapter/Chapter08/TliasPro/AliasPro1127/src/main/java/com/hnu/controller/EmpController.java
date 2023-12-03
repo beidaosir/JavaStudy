952d5c34ce0e0e02540001bc3d8c7923f502d3b6
beidaosir <1360860184@qq.com>
2023-12-03 15:28:09 +0800

    添加一些内容

--- AllChapter/Chapter08/TliasPro/AliasPro1127/src/main/java/com/hnu/controller/EmpController.java
+++ AllChapter/Chapter08/TliasPro/AliasPro1127/src/main/java/com/hnu/controller/EmpController.java
@@ -1,8 +1,33 @@
 package com.hnu.controller;
 
+import com.hnu.pojo.PageBean;
+import com.hnu.pojo.Result;
+import com.hnu.service.EmpService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //员工管理控制器
+@Slf4j
 @RestController
+@RequestMapping("/emps")
 public class EmpController {
+
+    @Autowired
+    private EmpService empService;
+
+    //条件分页查询
+    @GetMapping
+    public Result page(@RequestParam(defaultValue = "1") Integer page,
+                       @RequestParam(defaultValue = "10") Integer pageSize) {
+        //记录日志
+        log.info("分页查询，参数：{},{}", page, pageSize);
+        //调用业务层分页查询功能
+        PageBean pageBean = empService.page(page, pageSize);
+        //响应
+        return Result.success(pageBean);
+    }
 }
--- AllChapter/Chapter08/TliasPro/AliasPro1127/src/main/java/com/hnu/service/EmpService.java
+++ AllChapter/Chapter08/TliasPro/AliasPro1127/src/main/java/com/hnu/service/EmpService.java
@@ -1,5 +1,14 @@
 package com.hnu.service;
 
+import com.hnu.pojo.PageBean;
+
 //员工业务规则
 public interface EmpService {
+    /**
+     * 条件分页查询
+     * @param page 页码
+     * @param pageSize 每页展示记录数
+     * @return
+     */
+    PageBean page(Integer page, Integer pageSize);
 }
