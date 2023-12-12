c0851c89e6e7f8a0c6dd4d5813136dc48c1be78f
beidaosir <1360860184@qq.com>
2023-12-12 22:25:50 +0800

    新建一个项目文件

---
+++ 实训/2023_12/2023_12_12/SpringBoot1212/src/main/java/com/hnu/HelloApplication.java
@@ -0,0 +1,13 @@
+package com.hnu;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
