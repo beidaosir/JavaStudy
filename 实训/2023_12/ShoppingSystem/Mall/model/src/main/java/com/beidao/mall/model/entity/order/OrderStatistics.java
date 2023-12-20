5440985df61a4b648cf43dce381b785626eb46f0
beidaosir <1360860184@qq.com>
2023-12-20 08:29:29 +0800

    构建前后端工程

---
+++ 实训/2023_12/ShoppingSystem/Mall/model/src/main/java/com/beidao/mall/model/entity/order/OrderStatistics.java
@@ -0,0 +1,17 @@
+package com.beidao.mall.model.entity.order;
+
+
+import com.beidao.mall.model.entity.base.BaseEntity;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class OrderStatistics extends BaseEntity {
+
+    private Date orderDate;
+    private BigDecimal totalAmount;
+    private Integer totalNum;
+    
+}
