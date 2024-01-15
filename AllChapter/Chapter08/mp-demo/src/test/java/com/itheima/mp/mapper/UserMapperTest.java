82a911673559c19c8051c366d328a70287f0245e
beidaosir <1360860184@qq.com>
2024-01-15 11:27:10 +0800

    update

--- AllChapter/Chapter08/mp-demo/src/test/java/com/itheima/mp/mapper/UserMapperTest.java
+++ AllChapter/Chapter08/mp-demo/src/test/java/com/itheima/mp/mapper/UserMapperTest.java
@@ -1,5 +1,8 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.itheima.mp.domain.po.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,4 +56,71 @@ class UserMapperTest {
     void testDeleteUser() {
         userMapper.deleteById(5L);
     }
+
+
+    @Test
+    void testQueryWrapper(){
+        //1、构建查询条件
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .select("id","username","info","balance")
+                .like("username","o")
+                .ge("balance",1000);
+
+        //2、查询
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
+
+
+    }
+
+
+    @Test
+    void testLambdaQueryWrapper(){
+        //1、构建查询条件
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<User>()
+                .select(User::getId,User::getUsername,User::getInfo,User::getBalance)
+                .like(User::getUsername,"o")
+                .ge(User::getBalance,1000);
+
+        //2、查询
+        List<User> users = userMapper.selectList(wrapper);
+        users.forEach(System.out::println);
+
+
+    }
+
+
+    @Test
+    void testUpdateByQueryWrapper()
+    {
+        //1、要更新的数据
+        User user = new User();
+        user.setBalance(2000);
+
+        //2、更新条件
+        QueryWrapper<User> wrapper = new QueryWrapper<User>()
+                .eq("username","jack");
+
+        //3、执行更新
+        userMapper.update(user,wrapper);
+
+    }
+
+
+    @Test
+    void testUpdateWrapper()
+    {
+
+        List<Long> ids = List.of(1L,2L,3L);
+        UpdateWrapper<User> wrapper = new UpdateWrapper<User>()
+                .setSql("balance = balance - 200")
+                .in("id",ids);
+
+        //3、执行更新
+        userMapper.update(null,wrapper);
+
+    }
+
+
+
 }
