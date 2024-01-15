e89daae995d6cd654e8ce8ed24fbd064f2d2beb2
beidaosir <1360860184@qq.com>
2024-01-15 10:45:37 +0800

    update

--- AllChapter/Chapter08/mp-demo/src/main/java/com/itheima/mp/domain/po/User.java
+++ AllChapter/Chapter08/mp-demo/src/main/java/com/itheima/mp/domain/po/User.java
@@ -1,5 +1,7 @@
 package com.itheima.mp.domain.po;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,6 +12,7 @@ public class User {
     /**
      * 用户id
      */
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     /**
