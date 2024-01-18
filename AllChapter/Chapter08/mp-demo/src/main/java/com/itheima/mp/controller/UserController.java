7e36645f0f807722f84855acc31fc5f33bf0cec0
beidaosir <1360860184@qq.com>
2024-01-18 20:47:25 +0800

    update

--- AllChapter/Chapter08/mp-demo/src/main/java/com/itheima/mp/controller/UserController.java
+++ AllChapter/Chapter08/mp-demo/src/main/java/com/itheima/mp/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.itheima.mp.controller;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.itheima.mp.domain.dto.UserFormDTO;
 import com.itheima.mp.domain.po.User;
+import com.itheima.mp.domain.query.UserQuery;
 import com.itheima.mp.domain.vo.UserVO;
 import com.itheima.mp.service.IUserService;
 import io.swagger.annotations.Api;
@@ -58,6 +61,27 @@ public class UserController {
     public void deductBalance(@PathVariable("id") Long id,
                               @PathVariable("money")Integer money){
         userService.deductBalance(id, money);
+    }
+
+
+
+    @GetMapping("/list")
+    @ApiOperation("根据id集合查询用户")
+    public List<UserVO> queryUsers(UserQuery query){
+        // 1.组织条件
+        String username = query.getName();
+        Integer status = query.getStatus();
+        Integer minBalance = query.getMinBalance();
+        Integer maxBalance = query.getMaxBalance();
+        LambdaQueryWrapper<User> wrapper = new QueryWrapper<User>().lambda()
+                .like(username != null, User::getUsername, username)
+                .eq(status != null, User::getStatus, status)
+                .ge(minBalance != null, User::getBalance, minBalance)
+                .le(maxBalance != null, User::getBalance, maxBalance);
+        // 2.查询用户
+        List<User> users = userService.list(wrapper);
+        // 3.处理vo
+        return BeanUtil.copyToList(users, UserVO.class);
     }
 
 }
