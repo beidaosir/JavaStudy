9304c71b852106715836564a98e786548fbccaa0
beidaosir <1360860184@qq.com>
2024-01-18 21:44:43 +0800

    update

--- AllChapter/Chapter08/mp-demo/src/main/java/com/itheima/mp/service/impl/UserServiceImpl.java
+++ AllChapter/Chapter08/mp-demo/src/main/java/com/itheima/mp/service/impl/UserServiceImpl.java
@@ -5,12 +5,14 @@ import com.itheima.mp.domain.po.User;
 import com.itheima.mp.mapper.UserMapper;
 import com.itheima.mp.service.IUserService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
     //扣减用户余额
     @Override
+    @Transactional
     public void deductBalance(Long id, Integer money) {
         // 1.查询用户
         User user = getById(id);
@@ -22,7 +24,13 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         if (user.getBalance() < money) {
             throw new RuntimeException("用户余额不足");
         }
-        // 4.扣减余额
-        baseMapper.deductMoneyById(id, money);
+        // 4.扣减余额 update tb_user set balance = balance - ?
+        int remainBalance = user.getBalance() - money;
+        lambdaUpdate()
+                .set(User::getBalance, remainBalance) // 更新余额
+                .set(remainBalance == 0, User::getStatus, 2) // 动态判断，是否更新status
+                .eq(User::getId, id)
+                .eq(User::getBalance, user.getBalance()) // 乐观锁
+                .update();
     }
 }
