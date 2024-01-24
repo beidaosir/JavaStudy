be2ca91ca8429409016cd7ee832a021e8c382950
beidaosir <1360860184@qq.com>
2024-01-24 21:16:24 +0800

    Update PageDTO.java

--- AllChapter/Chapter08/mp-demo/src/main/java/com/itheima/mp/domain/dto/PageDTO.java
+++ AllChapter/Chapter08/mp-demo/src/main/java/com/itheima/mp/domain/dto/PageDTO.java
@@ -11,6 +11,8 @@ import lombok.Data;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Data
 @ApiModel(description = "分页结果")
@@ -45,5 +47,33 @@ public class PageDTO<T> {
         return dto;
 
     }
+
+
+
+    public static <PO,VO>  PageDTO<VO> of(Page<PO> p, Function<PO,VO> convertor){
+
+        PageDTO<VO> dto = new PageDTO<>();
+        //3.1总条数
+        dto.setTotal(p.getTotal());
+        //3.2总页数
+        dto.setPages(p.getPages());
+        //3.3当前页数据
+        List<PO> records = p.getRecords();
+        if (CollUtil.isEmpty(records)){
+            dto.setList(Collections.emptyList());
+            return dto;
+        }
+
+        //3.4拷贝user的vo
+        List<VO> collect = records.stream().map(convertor).collect(Collectors.toList());
+        //List<VO> vos = BeanUtil.copyToList(records,clazz);
+        dto.setList(collect);
+
+        //4.返回
+        return dto;
+
+    }
+
+}
 
 }
