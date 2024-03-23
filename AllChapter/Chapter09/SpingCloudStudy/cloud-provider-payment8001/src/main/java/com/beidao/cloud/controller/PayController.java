0afdaf685b8dd54b8275c6bc3c6a3d293b7d6ead
beidaosir <1360860184@qq.com>
2024-03-23 22:15:21 +0800

    Update PayController.java

--- AllChapter/Chapter09/SpingCloudStudy/cloud-provider-payment8001/src/main/java/com/beidao/cloud/controller/PayController.java
+++ AllChapter/Chapter09/SpingCloudStudy/cloud-provider-payment8001/src/main/java/com/beidao/cloud/controller/PayController.java
@@ -3,6 +3,7 @@ package com.beidao.cloud.controller;
 
 import com.beidao.cloud.entities.Pay;
 import com.beidao.cloud.entities.PayDTO;
+import com.beidao.cloud.resp.ResultData;
 import com.beidao.cloud.service.PayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -24,38 +25,41 @@ public class PayController {
 
     @PostMapping(value = "/pay/add")
     @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
-    public String addPay(@RequestBody Pay pay) {
+    public ResultData<String> addPay(@RequestBody Pay pay) {
 
         System.out.println(pay.toString());
         int i = payService.add(pay);
-        return "成功插入记录，返回值：" + i;
+        return ResultData.success("成功插入记录，返回值：" + i);
 
     }
 
 
     @DeleteMapping(value = "/pay/del/{id}")
     @Operation(summary = "删除",description = "删除支付流水方法")
-    public Integer deletePay(@PathVariable("id") Integer id){
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id){
 
-        return payService.delete(id);
+        int i = payService.delete(id);
+        return ResultData.success(i);
     }
 
     @PutMapping(value = "/pay/update")
     @Operation(summary = "修改",description = "修改支付流水方法")
-    public String updatePay(@RequestBody PayDTO payDTO){
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO){
 
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO,pay);
         int i = payService.update(pay);
 
-        return "成功修改记录，返回值：" + i;
+        return ResultData.success("成功修改记录，返回值：" + i);
     }
 
     @GetMapping(value = "/pay/get/{id}")
     @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
-    public Pay getById(@PathVariable("id") Integer id){
+    public ResultData<Pay> getById(@PathVariable("id") Integer id){
 
-        return payService.getById(id);
+        Pay pay = payService.getById(id);
+
+        return ResultData.success(pay);
     }
 
 
