6ce02098d30dbeac9e139e2a8cf7f2a6bb88369c
beidaosir <1360860184@qq.com>
2024-04-03 23:28:34 +0800

    动态获取线上的服务列表

--- AllChapter/Chapter09/SpingCloudStudy/cloud-consumer-order80/src/main/java/com/beidao/cloud/controller/OrderController.java
+++ AllChapter/Chapter09/SpingCloudStudy/cloud-consumer-order80/src/main/java/com/beidao/cloud/controller/OrderController.java
@@ -4,12 +4,16 @@ import com.beidao.cloud.entities.PayDTO;
 import com.beidao.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class OrderController
@@ -45,5 +49,27 @@ public class OrderController
     private String getInfoByConsul()
     {
         return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/info", String.class);
+    }
+
+
+    //动态获取线上的服务列表
+    @Resource
+    private DiscoveryClient discoveryClient;
+    @GetMapping("/consumer/discovery")
+    public String discovery()
+    {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            System.out.println(element);
+        }
+
+        System.out.println("===================================");
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance element : instances) {
+            System.out.println(element.getServiceId()+"\t"+element.getHost()+"\t"+element.getPort()+"\t"+element.getUri());
+        }
+
+        return instances.get(0).getServiceId()+":"+instances.get(0).getPort();
     }
 }
