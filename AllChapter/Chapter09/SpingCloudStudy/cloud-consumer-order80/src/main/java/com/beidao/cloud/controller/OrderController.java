9c31c46f91fe2740da4f7eed27db451ee308332a
beidaosir <1360860184@qq.com>
2024-04-03 23:16:25 +0800

    负载均衡测试

--- AllChapter/Chapter09/SpingCloudStudy/cloud-consumer-order80/src/main/java/com/beidao/cloud/controller/OrderController.java
+++ AllChapter/Chapter09/SpingCloudStudy/cloud-consumer-order80/src/main/java/com/beidao/cloud/controller/OrderController.java
@@ -12,39 +12,38 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class OrderController {
+public class OrderController
+{
+    //public static final String PaymentSrv_URL = "http://localhost:8001";//先写死，硬编码
 
-   // public static final String PaymentSrv_URL = "http://localhost:8001";//先写死，硬编码
-    public static final String PaymentSrv_URL = "http://localhost:cloud-payment-service";//服务注册中心上面的微服务名称
+    public static final String PaymentSrv_URL = "http://cloud-payment-service";//服务注册中心上的微服务名称
 
-    @Resource
+    @Autowired
     private RestTemplate restTemplate;
 
     /**
      * 一般情况下，通过浏览器的地址栏输入url，发送的只能是get请求
-     * 我们底层调用的是post方法，模拟消费者发送get请求，客户端消费者
-     * 参数可以不添加@RequestBody
-     *
+     * 我们模拟消费者发送get请求，but底层调用post方法，客户端消费者参数PayDTO可以不添加@RequestBody
      * @param payDTO
      * @return
      */
     @GetMapping("/consumer/pay/add")
-    public ResultData addOrder(PayDTO payDTO) {
-        return restTemplate.postForObject(PaymentSrv_URL + "/pay/add", payDTO, ResultData.class);
+    public ResultData addOrder(PayDTO payDTO)
+    {
+        return restTemplate.postForObject(PaymentSrv_URL + "/pay/add",payDTO,ResultData.class);
     }
 
-    // 删除+修改操作作为家庭作业
+    // 删除+修改操作作为家庭作业，O(∩_∩)O。。。。。。。
+
     @GetMapping("/consumer/pay/get/{id}")
-    public ResultData getPayInfo(@PathVariable("id") Integer id) {
-        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/" + id, ResultData.class, id);
+    public ResultData getPayInfo(@PathVariable Integer id)
+    {
+        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/"+id, ResultData.class, id);
     }
 
-   /* @GetMapping("/consumer/pay/del")
-    public ResponseEntity<ResultData> deletePayInfo(PayDTO payDTO) {
-        ResponseEntity<ResultData> response = restTemplate.exchange(PaymentSrv_URL + "/pay/del/",
-                HttpMethod.DELETE, new HttpEntity<>(payDTO), ResultData.class);
-        return response;
-    }*/
-
-
+    @GetMapping(value = "/consumer/pay/get/info")
+    private String getInfoByConsul()
+    {
+        return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/info", String.class);
+    }
 }
